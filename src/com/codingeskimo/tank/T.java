681f0aa3447eeffa598068c715f41c483a54c645
codingEskimo <codingEskimo@users.noreply.github.com>
2020-05-12 17:18:54 -0400

    Rewrite T and Frame, and add square

--- src/com/codingeskimo/tank/T.java
+++ src/com/codingeskimo/tank/T.java
@@ -6,18 +6,8 @@ import java.awt.event.WindowEvent;
 
 public class T {
     public static void main(String[] args) {
-        Frame f  = new Frame();
-        f.setSize(800, 600);
-        f.setResizable(false);
-        f.setTitle("Tank War");
-        f.setVisible(true);
+        TankFrame f  = new TankFrame();
 
-        f.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
 
     }
 }
