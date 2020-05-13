270e732fc2734420cdc051b48b39129ab51ca6e7
codingEskimo <codingEskimo@users.noreply.github.com>
2020-05-13 17:36:42 -0400

    Solving freshing problem on screen

--- src/com/codingeskimo/tank/Tank.java
+++ src/com/codingeskimo/tank/Tank.java
@@ -25,7 +25,10 @@ public class Tank {
     }
 
     public void paint(Graphics g) {
+        Color originColor = g.getColor();
+        g.setColor(Color.YELLOW);
         g.fillRect(x, y, 50, 50);
+        g.setColor(originColor);
         moving();
     }
 
--- src/com/codingeskimo/tank/TankFrame.java
+++ src/com/codingeskimo/tank/TankFrame.java
@@ -9,9 +9,10 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
     Tank myTank = new Tank(200, 200, Dir.DOWN, false);
     Bullet myBullet = new Bullet(300, 300, Dir.DOWN);
+    private static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
 
     public TankFrame() {
-        setSize(800, 600);
+        setSize(GAME_WIDTH, GAME_HEIGHT);
         setResizable(false);
         setTitle("Tank War");
         setVisible(true);
@@ -24,6 +25,27 @@ public class TankFrame extends Frame {
             }
         });
     }
+
+    /***
+     * The code blew to solving fleshing problem
+     * In order to do that, draw everything in memory and paint it once.
+     * update method will be called before update, So we capture it and call our own method
+     ***/
+    Image offScreenImage = null;
+    @Override
+    public void update(Graphics g) {
+        if (offScreenImage == null) {
+            offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+        }
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        Color c = gOffScreen.getColor();
+        gOffScreen.setColor(Color.BLACK);
+        gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage, 0, 0, null);
+    }
+
 
     @Override
     public void paint(Graphics g) {
