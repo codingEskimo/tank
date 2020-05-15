f1addb19ddffe65ab0236132fec4dc77d34dea2c
codingEskimo <codingEskimo@users.noreply.github.com>
2020-05-14 23:34:40 -0400

    Add unit test

---
+++ src/test/ImageTest.java
@@ -0,0 +1,25 @@
+package test;
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ImageTest {
+    @Test
+    void test() {
+        try {
+            BufferedImage image = ImageIO.read(
+                    new File("D:\\Study\\src\\java_fundation\\tank\\src\\images\\bulletD.gif"));
+            assertNotNull(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
